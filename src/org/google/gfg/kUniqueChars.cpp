4324c491c19f0e1bae21357e84d5d6bbc4e0e147
Prakhar Dhama <prakhardhama@gmail.com>
2019-06-03 09:34:18 +0530

    # smaller code for atmost kuniquechars

--- src/org/google/gfg/kUniqueChars.cpp
+++ src/org/google/gfg/kUniqueChars.cpp
@@ -6,30 +6,21 @@ using namespace std;
 // longest substring with k unique characters
 void solve(string str, int K) {
     printf("input = %s\n", str.c_str());
-    unordered_map<char, int> hash;
-    int ans = 0, N = str.size();
-    int prev = 0, current = 0;
-    for(int i = 0; i < N; ++i) {
-        // printf("index = %d => ", i);
-        // for(auto h: hash) if(h.first >= 'a' && h.first <= 'z') printf("%c(%d) ", h.first, h.second); printf("\n");
-        if(hash.find(str[i]) == hash.end() || hash[str[i]] == 0) {
-            hash[str[i]] = 1; ++current;
-        } else {
-            hash[str[i]]++;
-        }
-        while(current > K && prev < i) {
-            hash[str[prev]]--;
-            if(hash[str[prev]] == 0) --current;
-            prev++;
-        }
-        if(current == K) ans = max(ans, i - prev + 1);      // printf("found at (%d, %d)\n", prev, i);
+    int ans = 0;
+    unordered_map<char, int> mp; // count of each str[i]
+    for(int ct = 0, l = 0, r = 0; r < str.size(); ++r) {
+        char ch = str[r];
+        if(mp.find(ch) == mp.end() || mp[ch] == 0) mp[ch] = 1, ++ct;
+        else ++mp[ch];
+        if(ct == K+1 && --ct) while(--mp[str[l++]] > 0);
+        ans = max(ans, r-l+1);
     }
     printf("ans = %d\n", ans);
 }
 
 int main() {
     // freopen("kUniqueChars.out", "w", stdout);
-    solve("aabbcc", 3);
+    solve("aabbccddd", 2);
     solve("abcccdefghefgh", 4);
     return 0;
 }
